c14cc566a83eb347bee285d73ea71d01dd354d7f
BellaMamedov <94701315+BellaMamedov@users.noreply.github.com>
2023-04-24 11:36:10 +0300

    addes index file to login page

--- pop_shop_match_server/api/index.js
+++ pop_shop_match_server/api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt.js");
+const bcrypt = require("bcrypt");
 const User = require("./models/user.js");
 require("dotenv").config();
 const app = express();
