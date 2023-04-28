c5b3e848d853ed13081f6a6250cc41b25b1fe03d
BellaMamedov <94701315+BellaMamedov@users.noreply.github.com>
2023-04-28 12:49:51 +0300

    Merge

---
+++ pop_shop_match_client/src/components/AddedNewAsset/added-new-asset.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import "./added-new-asset.scss";
+
+const AddedNewAsset = () => {
+  {
+    const [address, setAdress] = useState("");
+    const [city, setCity] = useState("");
+    const [addedPhotos, setAddedPhotos] = useState([]);
+    const [photoLink, setPhotoLink] = useState("");
+    const [description, setDescription] = useState("");
+
+    const onChangeHandlerAddress = (event) => {
+      setAdress(event.target.value);
+    };
+
+    const onChangeHandlerCity = (event) => {
+      setCity(event.target.value);
+    };
+
+    const onChangeHandlerPhotoLink = (event) => {
+      setPhotoLink(event.target.value);
+    };
+
+    const onChangeHandlerDescription = (event) => {
+      setDescription(event.target.value);
+    };
+
+    return (
+      <div className="all-form-body">
+        <div className="new-asset-form-wrapper">
+          <form>
+            <h2>Let's get started listing your space.</h2>
+            <p>Your space characteristics</p>
+            <div className="input-box">
+              <input
+                type="text"
+                value={address}
+                onChange={onChangeHandlerAddress}
+                placeholder="exmple: Rothschild Boulevard 54"
+              />
+              <label className="store-address">
+                What is the address of your space?
+              </label>
+            </div>
+            <div className="input-box">
+              <input
+                type="text"
+                value={city}
+                onChange={onChangeHandlerCity}
+                placeholder="city, for example: Tel Aviv"
+              />
+              <label className="store-city">Store city</label>
+            </div>
+            <p>Showcase your space</p>
+            <div className="input-box">
+              <input
+                className="add-link-photo-input"
+                type="text"
+                value={photoLink}
+                onChange={onChangeHandlerPhotoLink}
+                placeholder={"add using a link.....jpg"}
+              />
+              <button className="add-photo-btn">Add photo</button>
+              <label className="store-photos">Add photo by link </label>
+              <div className="add-photo-link-btn-div"></div>
+            </div>
+            <div className="asset-photos grid">
+              <button className="upload-photos-btn">
+                Upload from your device
+                <AiOutlineCloudUpload className="icon" />
+              </button>
+            </div>
+            <p className="mini-place-desc">What best describes your space?</p>
+            <textarea
+              rows={7}
+              cols={60}
+              value={description}
+              onChange={onChangeHandlerDescription}
+            />
+            <div className="save-btn-div">
+              <button className="save-form-new-asset-btn" type="submit">
+                save
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default AddedNewAsset;
--- pop_shop_match_client/src/index.js
+++ pop_shop_match_client/src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ArtistApp from "./pages/ArtisApp/artist-app";
 import LoginPage from "./pages/LoginPage/login-page";
 import RegisterPage from "./pages/RegisterPage/register-page";
+import NewAssetApp from "./pages/NewAssetApp/new-asset-app";
 import axios from "axios";
 
 
-axios.defaults.baseURL = 'http://localhost:4000/';
+axios.defaults.baseURL = 'http://127.0.0.1:4000/';
+axios.defaults.withCredentials = true;
 
 
 
@@ -20,6 +22,7 @@ const App = () => {
         <Route path="/" element={<ArtistApp />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/add-new-asset" element={<NewAssetApp />} />
       </Routes>
     </BrowserRouter>
   );
